2c766d445ce753f231275f0acfbbd1f7a39d22c6
Daniel Turner <danielturner184@gmail.com>
2020-08-13 21:03:28 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
+    
 
 end
 
@@ -36,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -52,10 +55,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,7 +84,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] ={ 
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }  
+ programmer_hash
 end
 
 def changing_alan
@@ -95,7 +102,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
@@ -103,7 +110,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash
 end
 
 def adding_to_dennis
@@ -122,8 +129,8 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C","Assembly"]
       }
     }
-
+programmer_hash
 end
